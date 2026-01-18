7a4fa12d93cc2f71a9fb878ef2cdf20ca5f2f57d
Heptadecagon <123870086+17gon@users.noreply.github.com>
2026-01-18 00:13:10 +0000

    Add comment
    Adding comment -> read the commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+//Seems we need to make a push request to deploy it on GithubActions, i hope it will work
 // https://vite.dev/config/
 export default defineConfig({
   base: '/VueProject/',
